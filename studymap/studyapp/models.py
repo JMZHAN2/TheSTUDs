3cee49eaf524dae235f63fc9a2b83324b23a4212
Alex Lucas <alex.lucas@emory.edu>
2024-10-11 16:58:45 -0400

    added StudyStreak class but didn't get far on implementing it

--- studymap/studyapp/models.py
+++ studymap/studyapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Stopwatch(models.Model):
@@ -9,3 +10,12 @@ class Stopwatch(models.Model):
     
     def __str__(self):
         return f"{self.time_spent}"
+
+class StudyStreak(models.Model):
+    users = models.ForeignKey(User, on_delete = models.CASCADE)
+    streak_days = models.PositiveIntegerField(default=0)  
+    start_date = models.DateField(null=True, blank=True) 
+    last_study_date = models.DateField(null=True, blank=True)  
+
+    def __str__(self):
+        return f"{self.user.username} - Streak: {self.streak_days} days"
