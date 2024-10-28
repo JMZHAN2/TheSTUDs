da8cf451f11fcfc84eb94081bd01cb3bf029dcff
Alex Lucas <alex.lucas@emory.edu>
2024-10-28 11:08:28 -0400

    fixed study streak time zones

--- studymap/studyapp/models.py
+++ studymap/studyapp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Stopwatch(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     time_spent = models.IntegerField(default=0)
-    time_start = models.DateTimeField(auto_now_add=True)
+    time_start = models.DateTimeField(default = timezone.now)
     title = models.CharField(max_length=40)
     def __str__(self):
         return f"{self.title}: {self.time_spent} seconds"
@@ -19,17 +20,20 @@ class Stopwatch(models.Model):
     def calculate_study_streak(user):
         # retrieve all study sessions and order by start time
         sessions = Stopwatch.objects.filter(user=user).order_by('-time_start')
-
+        unique_study_dates = set()  # Track unique study dates
         streak = 0
         last_study_date = None
         for session in sessions:
             study_date = session.time_start.date()
-            if last_study_date is None or (last_study_date - study_date).days == 1:
-                streak += 1
-                last_study_date = study_date
-            else:
-                break  # stop counting if there's a gap
+            if study_date not in unique_study_dates:
+                unique_study_dates.add(study_date)
+                if last_study_date is None or (last_study_date - study_date).days == 1:
+                    streak += 1
+                    last_study_date = study_date
+                else:
+                    break  # stop counting if there's a gap
         return streak
+        
 
 
 
